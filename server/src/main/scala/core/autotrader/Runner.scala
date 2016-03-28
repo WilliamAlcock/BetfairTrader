a99b6ee4ef1092de7c9d81b84452ee90302dfbc2
William Alcock <wjba20@sussex.ac.uk>
2016-03-28 23:47:15 +0100

    Finished Autotrader Runner

--- server/src/main/scala/core/autotrader/Runner.scala
+++ server/src/main/scala/core/autotrader/Runner.scala
@@ -17,11 +17,13 @@ class Runner(controller: ActorRef) extends FSM[State, Data] with Stash {
 
   def getTimeTill(time: DateTime): FiniteDuration = Math.max(1, time.getMillis - DateTime.now().getMillis) millis
 
-  def getPosition(orders: List[OrderData]): Double = orders.map(_.order match {
-    case Some(x) if x.side == Side.LAY => x.sizeRemaining
-    case Some(x) if x.side == Side.BACK => -x.sizeRemaining
-    case _ => 0.0
-  }).sum
+  // The following three functions have been wrapped so calling them can be tested
+
+  def unstashAllMsg(): Unit = unstashAll()
+
+  def stashMsg(): Unit = stash()
+
+  def resendMarketBook(s: Strategy): Unit = if (s.marketBook.isDefined) self ! s.marketBook.get
 
   startWith(Idle, NoData)
 
@@ -45,46 +47,46 @@ class Runner(controller: ActorRef) extends FSM[State, Data] with Stash {
   }
 
   when(Initialised) {
-    case Event(e: MarketBookUpdate, strategy: Strategy) =>
-      stay using strategy.copy(marketBook = Some(e))
+    case Event(e: MarketBookUpdate, strategy: Strategy) => stay using strategy.copy(marketBook = Some(e))
 
-    case Event(Start, strategy: Strategy) =>
+    case Event(Start, s: Strategy) =>
       // If a stopTime is defined set a timer
-      if (strategy.stopTime.isDefined) {
-        setTimer("StopTimer", Stop, getTimeTill(strategy.stopTime.get))
+      if (s.stopTime.isDefined) {
+        setTimer("StopTimer", Stop, getTimeTill(s.stopTime.get))
       }
-      goto (ConfirmingOrder) using strategy
-  }
-
-  onTransition {
-    case _ -> ConfirmingOrder =>
-      nextStateData match {
-        case x: Strategy => controller ! PlaceOrders(x.marketId, Set(x.instructions.head))
-      }
+      controller ! PlaceOrders(s.marketId, Set(s.instructions.head))
+      goto (ConfirmingOrder) using s
   }
 
   when(ConfirmingOrder) {
-    case Event(PlaceExecutionReportContainer(result), strategy: Strategy) if result.status == ExecutionReportStatus.SUCCESS =>       // Order Confirmed -> Save Id -> WaitingForOrder
-      unstashAll()
-      goto (WaitingForOrder) using strategy.copy(
-        instructions = strategy.instructions.tail,
-        orders = OrderData(result.instructionReports.head.betId.get) :: strategy.orders
+    case Event(PlaceExecutionReportContainer(result), s: Strategy) if result.status == ExecutionReportStatus.SUCCESS =>       // Order Confirmed -> Save Id -> WaitingForOrder
+      unstashAllMsg()
+      resendMarketBook(s)
+      goto (WaitingForOrder) using s.copy(
+        instructions = s.instructions.tail,
+        orders = OrderData(result.instructionReports.head.betId.get) :: s.orders
       )
-    case Event(OrderManagerException(_), strategy: Strategy)          => goto (ConfirmingOrder)       // Order Failed
-    case Event(PlaceExecutionReportContainer(_), strategy: Strategy)  => goto (ConfirmingOrder)       // Order Failed
-    case Event(x: MarketBookUpdate, strategy: Strategy)               => stay using strategy.copy(marketBook = Some(x))        // Update internal state of the marketBook
+    case Event(OrderManagerException(_), s: Strategy) =>                  // Order Failed
+      controller ! PlaceOrders(s.marketId, Set(s.instructions.head))
+      stay()
+    case Event(PlaceExecutionReportContainer(_), s: Strategy) =>          // Order Failed
+      controller ! PlaceOrders(s.marketId, Set(s.instructions.head))
+      stay()
+    case Event(x: MarketBookUpdate, strategy: Strategy) => stay using strategy.copy(marketBook = Some(x))        // Update internal state of the marketBook
     case Event(e, s) =>                 // Stash any other changes until confirmation has been received
       println("Stashing", e, s)
-      stash()
+      stashMsg()
       stay()
   }
 
-  onTransition {
-    case _ -> WaitingForOrder =>
-      // Replay the last version of the marketBook received as it may trigger the stop
-      nextStateData match {
-        case s: Strategy => if (s.marketBook.isDefined) self ! s.marketBook.get
-      }
+  def isStopTriggered(s: Strategy, m: MarketBook): Boolean = {
+    val backPrice = m.getRunner(s.selectionId).backPrice
+    val layPrice = m.getRunner(s.selectionId).layPrice
+    (s.stopSide, backPrice, layPrice) match {
+      case (Side.BACK, Some(_backPrice), _) if _backPrice <= s.stopPrice  => true
+      case (Side.LAY, _, Some(_layPrice))   if _layPrice >= s.stopPrice   => true
+      case _ => false
+    }
   }
 
   when (WaitingForOrder) {
@@ -92,93 +94,74 @@ class Runner(controller: ActorRef) extends FSM[State, Data] with Stash {
     case Event(e: OrderUpdated, s: Strategy) if e.order.betId == s.orders.head.id => stay using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail)
     case Event(e: OrderExecuted, s: Strategy) if e.order.betId == s.orders.head.id =>
       if (s.instructions.isEmpty) {
-        stop using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail, instructions = s.instructions.tail)
+        goto (Finished) using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail)
       } else {
-        goto (ConfirmingOrder) using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail, instructions = s.instructions.tail)
+        controller ! PlaceOrders(s.marketId, Set(s.instructions.head))
+        goto (ConfirmingOrder) using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail)
       }
 
     case Event(e: MarketBookUpdate, s: Strategy) =>
-      (s.stopSide, e.data.getRunner(s.selectionId).backPrice, e.data.getRunner(s.selectionId).layPrice) match {
-        case (Side.BACK, Some(backPrice), _) if backPrice <= s.stopPrice  => goto (ConfirmingStopOrder) using s.copy(marketBook = Some(e))
-        case (Side.LAY, _, Some(layPrice))   if layPrice >= s.stopPrice   => goto (ConfirmingStopOrder) using s.copy(marketBook = Some(e))
-        case _ => stay using s.copy(marketBook = Some(e))
+      if (!s.stoppingOut && isStopTriggered(s, e.data)) {
+        cancelTimer("StopTimer")
+        controller ! CancelOrders(s.marketId, Set(CancelInstruction(s.orders.head.id)))
+        goto (CancellingOrder) using s.copy(marketBook = Some(e))
+      } else {
+        stay using s.copy(marketBook = Some(e))
       }
-    case Event(Stop, s: Strategy) => goto (CancellingOrder)                                                             // Timer triggers stop
+    case Event(Stop, s: Strategy) =>
+      cancelTimer("StopTimer")
+      controller ! CancelOrders(s.marketId, Set(CancelInstruction(s.orders.head.id)))
+      goto (CancellingOrder)                                                             // Timer triggers stop
   }
 
-  onTransition {
-    case _ -> CancellingOrder =>
-      nextStateData match {
-        case s: Strategy =>
-          cancelTimer("StopTimer")
-          controller ! CancelOrders(s.marketId, Set(CancelInstruction(s.orders.head.id)))
-      }
-  }
+  def getPosition(orders: List[OrderData]): Double = orders.map(_.order match {
+    case Some(x) if x.side == Side.LAY => x.sizeMatched
+    case Some(x) if x.side == Side.BACK => -x.sizeMatched
+    case _ => 0.0
+  }).sum
 
-  def getStopInstruction(s: Strategy, size: Double): PlaceInstruction = {
-    val price = s.stopSide match {
-      case Side.BACK => 1.01
-      case Side.LAY  => 1000
-    }
-    PlaceInstruction(OrderType.LIMIT, s.selectionId, 0.0, s.stopSide, Some(LimitOrder(size, price, PersistenceType.PERSIST)))
+  def getStopInstruction(s: Strategy): Option[PlaceInstruction] = getPosition(s.orders) match {
+    case x if x > 0 => Some(PlaceInstruction(OrderType.LIMIT, s.selectionId, 0.0, Side.BACK, Some(LimitOrder(Math.abs(x), 1.01, PersistenceType.PERSIST))))
+    case x if x < 0 => Some(PlaceInstruction(OrderType.LIMIT, s.selectionId, 0.0, Side.LAY, Some(LimitOrder(Math.abs(x), 1000, PersistenceType.PERSIST))))
+    case _ => None
   }
 
   when(CancellingOrder) {
     case Event(CancelExecutionReportContainer(result), _) if result.status == ExecutionReportStatus.SUCCESS =>          // Order Cancelled, Wait for Execution Report
       stay()
-    case Event(OrderManagerException(_), _)          => goto(CancellingOrder)         // Cancel Failed
-    case Event(CancelExecutionReportContainer(_), _) => goto(CancellingOrder)         // Cancel Failed
-
-    case Event(e: MarketBookUpdate, s: Strategy) => stay using s.copy(marketBook = Some(e))
-    case Event(e, s) =>               // Stash any other changes until confirmation has been received
-      println("Stashing", e, s)
-      stash()
+    case Event(OrderManagerException(_), s: Strategy)          =>         // Cancel Failed
+      controller ! CancelOrders(s.marketId, Set(CancelInstruction(s.orders.head.id)))
       stay()
+    case Event(CancelExecutionReportContainer(_), s: Strategy) =>         // Cancel Failed
+      controller ! CancelOrders(s.marketId, Set(CancelInstruction(s.orders.head.id)))
+      stay()
+    case Event(e: MarketBookUpdate, s: Strategy) => stay using s.copy(marketBook = Some(e))
 
     case Event(e: OrderExecuted, s: Strategy) if e.order.betId == s.orders.head.id =>
       val orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail
-      val position = Math.abs(getPosition(orders))
-      if (position > 0) {
-        goto (ConfirmingOrder) using s.copy(instructions = List(getStopInstruction(s, position)))
+      val instruction = getStopInstruction(s.copy(orders = orders))
+
+      if (instruction.isDefined) {
+        controller ! PlaceOrders(s.marketId, Set(instruction.get))
+        goto (ConfirmingOrder) using s.copy(orders = orders, instructions = List(instruction.get))
       } else {
-        stop using s.copy(orders = orders)
+        goto (Finished) using s.copy(orders = orders)
       }
   }
 
   onTransition {
-    case _ -> ConfirmingOrder =>
-      nextStateData match {
-        case x: Strategy => controller ! PlaceOrders(x.marketId, Set(x.instructions.head))
-      }
+    case _ -> Finished =>
+      cancelTimer("StartTimer")
+      cancelTimer("StopTimer")
   }
 
-  when(ConfirmingStopOrder) {
-    case Event(PlaceExecutionReportContainer(result), s: Strategy) if result.status == ExecutionReportStatus.SUCCESS =>       // Order Confirmed
-      unstashAll()
-      goto (WaitingForStopOrder) using s.copy(
-        instructions = s.instructions.tail,
-        orders = OrderData(result.instructionReports.head.betId.get) :: s.orders
-      )
-    case Event(OrderManagerException(_), s: Strategy)          => goto (ConfirmingOrder)       // Order Failed
-    case Event(PlaceExecutionReportContainer(_), s: Strategy)  => goto (ConfirmingOrder)       // Order Failed
-    case Event(e: MarketBookUpdate, s: Strategy)               => stay using s.copy(marketBook = Some(e))        // Update internal state of the marketBook
-    case Event(e, s) =>                 // Stash any other changes until confirmation has been received
-      println("Stashing", e, s)
-      stash()
-      stay()
-  }
-
-  when(WaitingForStopOrder) {
-    case Event(e: OrderPlaced, s: Strategy) if e.order.betId == s.orders.head.id  => stay using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail)
-    case Event(e: OrderUpdated, s: Strategy) if e.order.betId == s.orders.head.id => stay using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail)
-    case Event(e: OrderExecuted, s: Strategy) if e.order.betId == s.orders.head.id =>
-      stop using s.copy(orders = s.orders.head.copy(order = Some(e.order)) :: s.orders.tail, instructions = s.instructions.tail)
-
-    case Event(e: MarketBookUpdate, s: Strategy) => stay using s.copy(marketBook = Some(e))
+  when(Finished) {
+    case Event(_, _) => stay()
   }
 
   onTermination {
-    case StopEvent(FSM.Normal, state, data) => println("STRATEGY STOPPED")
+    case StopEvent(_, _, _) =>
+      println("STRATEGY STOPPED")
   }
 }
 
@@ -189,8 +172,7 @@ object Runner {
   case object ConfirmingOrder extends State
   case object WaitingForOrder extends State
   case object CancellingOrder extends State
-  case object ConfirmingStopOrder extends State
-  case object WaitingForStopOrder extends State
+  case object Finished extends State
 
   case object Stop
   case object Start
@@ -206,12 +188,13 @@ object Runner {
   case class Strategy(marketId: String,
                       selectionId: Long,
                       marketBook: Option[MarketBookUpdate] = None,
-                      instructions: List[PlaceInstruction],
+                      instructions: List[PlaceInstruction] = List.empty,
                       orders: List[OrderData] = List.empty,
                       stopPrice: Double,
                       stopSide: Side,
-                      startTime: Option[DateTime],
-                      stopTime: Option[DateTime]) extends Data
+                      stoppingOut: Boolean = false,
+                      startTime: Option[DateTime] = None,
+                      stopTime: Option[DateTime] = None) extends Data
 
   def props(controller: ActorRef) = Props(new Runner(controller))
 }
